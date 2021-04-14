c53788b9d9f1798a128b17147b1dba879126bea1
Krishna Satya <vadapallikrishna@gmail.com>
2021-04-14 11:05:42 +0530

    updated

--- main.go
+++ main.go
@@ -2,72 +2,7 @@ package main
 
 import "fmt"
 
-type Container struct{
-    buffer int
-    size int
-}
 
-type Value struct {
-     reference Container
-     typeIdentifier string
-}
-
-type Error struct {
-}
-
-type Declaration struct {
-    typeIdentifier Value
-    name Value
-}
-
-type Unit struct {
-    identifier Value
-    method Value
-    args List
-}
-
-type Case struct {
-    literal Value
-     
-}
-
-type Loop struct {
-    literal Result
-}
-
-//module type
-//abstract module
-//design module
-//code module
-
-type Module struct {
-	
-}
-
-type Result struct {
-     call Unit
-     value Proof
-}
-
-type Variant struct {
-    result Result
-    ttype Type
-}
-
-type Proof struct {
-    sum Unit
-    leaf Unit
-}
-
-type Type struct {
-    dependentType Proof
-    result Result
-}
-
-type List struct {
-     key Type
-     value Type
-}
 
 type GraphVertice struct {
     vertice List
@@ -83,6 +18,20 @@ type Graph struct {
     edges []GraphEdge
 }
 
+type Unit struct {
+    present Quantifier
+    args List
+}
+
+type Function struct {
+    sum Unit
+    leaf Unit
+}
+
+type List struct {
+    key Function
+    value Function
+}
 
 
 func main() {
